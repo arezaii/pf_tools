72a13ba868501ef35ab1ce1de93cbfe0e445c700
Ahmad Rezaii <ahmadrezaii@u.boisestate.edu>
2020-06-18 15:47:57 -0600

    update compare based on new pfio code

--- f_to_c_compare.py
+++ f_to_c_compare.py
@@ -3,18 +3,19 @@ import time
 import pf_pytools.pf_fort_io as pf_fort_io
 import pfio
 
-input_pfb = '/home/arezaii/Downloads/CONUS.5layer.pfclm.run4.out.clm_output.00002.C.pfb'
+input_pfb = '/home/arezaii/data/CONUS.5layer.pfclm.run4.out.clm_output.00002.C.pfb'
 
 t3 = time.perf_counter()
-pfb_data_in = np.zeros((3342, 1888, 17), order='F')
-pf_fort_io.pfb_read(pfb_data_in, input_pfb)
-pfb_data_in = np.transpose(pfb_data_in, (2, 1, 0))
+fort_pfb_data = np.zeros((3342, 1888, 17), order='F')
+pf_fort_io.pfb_read(fort_pfb_data, input_pfb)
+fort_pfb_data = np.transpose(fort_pfb_data, (2, 1, 0))
 t4 = time.perf_counter()
 print(f'F load time {t4-t3:0.4f} seconds')
 
 t1 = time.perf_counter()
-pfb_read_rtn = pfio.pfread(input_pfb)
+c_pfb_data = pfio.pfread(input_pfb)
 t2 = time.perf_counter()
 print(f'C load time {t2-t1:0.4f} seconds')
 
-np.testing.assert_array_equal(pfb_data_in, pfb_read_rtn)
+if np.testing.assert_array_equal(fort_pfb_data, c_pfb_data) is None:
+    print('arrays are equal')
